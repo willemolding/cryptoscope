69e16b4885910eb25ac9fde82e826583b6a8c5a2
Willem Olding <willemolding@gmail.com>
2017-09-27 16:43:19 +1000

    adds react component for additive list element (untested)

--- settings/index.jsx
+++ settings/index.jsx
@@ -2,30 +2,71 @@ import * as api from "../common/api.js";
 
 console.log("Opening Settings page");
 
-var coinOptions = [];
-var baseOptions = [];
 
-function updateCoinOptions() {
-  return api.getCoinList()
-  .then(function(coins) {
-    coins = coins.sort(function compare(c1, c2) {
-      return c1["SortOrder"] - c2["SortOrder"];
+class AddNewPair extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {coinOptions: [], baseOptions: [], value=null};
+    updateCoinOptions();
+  }
+
+  updateCoinOptions() {
+    this.setState({coinOptions: []});
+
+    api.getCoinList()
+    .then(function(coins) {
+      coins = coins.sort(function compare(c1, c2) {
+        return c1["SortOrder"] - c2["SortOrder"];
+      });
+      coinOptions = coins.map(function(coinInfo){
+        return {name: coinInfo.Name + " - " + coinInfo.Symbol, value: coinInfo.Id};
+      });
+      this.setState({coinOptions: coinOptions});
+    })
+    .catch(function(err) {
+      console.log("Unable to access getCoinList API \n"+err);
     });
-    coinOptions = coins.map(function(coinInfo){
-      return {name: coinInfo.Name + " - " + coinInfo.Symbol, value: coinInfo.Id};
+  }
+
+  updateBaseOptions(id) {
+    this.setState({baseOptions: []});
+
+    api.getCoinSubscriptions(id)
+    .then(function(subs) {
+      baseOptions = subs.map(function(sub) {
+        return {name: sub.tsym +"  -  "+ sub.exchange, value: sub};
+      });
+      this.setState({baseOptions: baseOptions});
+    })
+    .catch(function(err) {
+      console.log("Unable to call getCoinSubscriptions API\n"+err);
     });
-  });
+  }
+
+  render() {
+    return (
+      <Section title="Add a new pair">
+        <Select 
+          label="Coin"
+          options={this.state.coinOptions}
+          onSelection={function(e) {
+            updateBaseOptions(e.values[0]);
+          }
+        }
+        />
+        <Select 
+          label="Base Currency/Exchange"
+          options={this.state.baseOptions}
+          onSelection={function(e) {
+            this.setState(value: e.values[0]);
+          }
+        }
+        />
+      </Section>
+    );
+  }
 }
-
-function updateBaseOptions(id) {
-  return api.getCoinSubscriptions(id)
-  .then(function(subs) {
-    baseOptions = subs.map(function(sub) {
-      return {name: sub.tsym +"  -  "+ sub.exchange, value: sub};
-    });
-  });
-}
-
 
 
 function mySettings(props) { 
@@ -38,26 +79,7 @@ function mySettings(props) {
           settingsKey="subscriptions"
           maxItems="10"
           addAction={
-            <Section title="Add a new pair">
-              <Select label="Coin"
-                  options={coinOptions}
-                  onSelection={
-                    (selection) => {
-                      updateBaseOptions(selection.values[0])
-                      .then(function() {
-                          registerSettingsPage(mySettings);
-                      })
-                      .catch(function(err) {
-                          console.log("Unable to access getCoinSubscriptions API \n"+err);
-                      });
-                    }
-                  }
-              />
-              <Select 
-                label="Base Currency/Exchange"
-                options={baseOptions}
-              />
-            </Section>
+            <AddNewPair/>
           }
         />
       </Section>
@@ -66,10 +88,5 @@ function mySettings(props) {
 }
 
 
-updateCoinOptions()
-.then(function() {
-  registerSettingsPage(mySettings);
-})
-.catch(function(err) {
-  console.log("Unable to access getCoinList API \n"+err);
-});
+registerSettingsPage(mySettings);
+
